afb4f37761582e23fe89ff815f408f524f66060e
Jack Butler <john.butler@uconn.edu>
2021-05-16 15:50:57 -0400

    update dist files

---
+++ dist/data_units_grunt_json.bundle.js
@@ -0,0 +1,14 @@
+(self["webpackChunkroguelike_js"] = self["webpackChunkroguelike_js"] || []).push([["data_units_grunt_json"],{
+
+/***/ "./data/units/grunt.json":
+/*!*******************************!*\
+  !*** ./data/units/grunt.json ***!
+  \*******************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = JSON.parse('{"name":"grunt","type":"HUMAN","sprite":"player","paletteSwaps":{"DARK_GREEN":"DARK_BROWN","GREEN":"DARK_BROWN","CYAN":"ORANGE"},"startingLife":50,"startingMana":null,"startingDamage":3,"minLevel":1,"maxLevel":4,"lifePerLevel":20,"manaPerLevel":null,"damagePerLevel":1,"equipment":["bronze_chain_mail","iron_helmet","bronze_sword"],"aiParameters":{"speed":0.95,"visionRange":8,"fleeThreshold":0.1}}');
+
+/***/ })
+
+}]);
---
+++ dist/data_units_soldier_json.bundle.js
@@ -0,0 +1,14 @@
+(self["webpackChunkroguelike_js"] = self["webpackChunkroguelike_js"] || []).push([["data_units_soldier_json"],{
+
+/***/ "./data/units/soldier.json":
+/*!*********************************!*\
+  !*** ./data/units/soldier.json ***!
+  \*********************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = JSON.parse('{"name":"ENEMY_SOLDIER","type":"HUMAN","sprite":"player","paletteSwaps":{"DARK_GREEN":"DARK_BLUE","GREEN":"DARK_BLUE","CYAN":"ORANGE"},"startingLife":60,"startingMana":null,"startingDamage":4,"minLevel":3,"maxLevel":6,"lifePerLevel":20,"manaPerLevel":null,"damagePerLevel":1,"equipment":["iron_chain_mail","iron_helmet","steel_sword"],"aiParameters":{"speed":0.95,"visionRange":10,"fleeThreshold":0.1}}');
+
+/***/ })
+
+}]);
