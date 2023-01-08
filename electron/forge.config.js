cdf219472ec65394338d104c2b75e1782532dfe1
Jack Butler <john.butler@uconn.edu>
2023-01-07 22:59:58 -0500

    add a bunch of ci steps

--- electron/forge.config.js
+++ electron/forge.config.js
@@ -2,21 +2,21 @@ module.exports = {
   packagerConfig: {},
   rebuildConfig: {},
   makers: [
-    {
+    /*{
       name: '@electron-forge/maker-squirrel',
       config: {},
-    },
+    },*/
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      platforms: ['darwin', 'win32'],
     },
-    {
+    /*{
       name: '@electron-forge/maker-deb',
       config: {},
     },
     {
       name: '@electron-forge/maker-rpm',
       config: {},
-    },
+    },*/
   ],
 };
