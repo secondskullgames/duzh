b021aa5e34a8763f1f546a7a1989122e11f31c32
John Butler <john.butler@uconn.edu>
2023-05-08 17:09:35 -0400

    cleanup

--- src/main/input/InputHandler.ts
+++ src/main/input/InputHandler.ts
@@ -1,25 +1,8 @@
-import { playSound } from '../sounds/SoundFX';
-import Sounds from '../sounds/Sounds';
-import Coordinates from '../geometry/Coordinates';
-import PlayerUnitController from '../entities/units/controllers/PlayerUnitController';
-import { toggleFullScreen } from '../utils/dom';
-import { checkNotNull } from '../utils/preconditions';
 import GameState from '../core/GameState';
-import type { ArrowKey, KeyCommand, ModifierKey, NumberKey } from './inputTypes';
-import { getDirection, mapToCommand } from './inputMappers';
-import MapFactory from '../maps/MapFactory';
+import type { KeyCommand } from './inputTypes';
+import { mapToCommand } from './inputMappers';
 import GameRenderer from '../graphics/renderers/GameRenderer';
 import { GameScreen } from '../types/types';
-import { playTurn } from '../actions/playTurn';
-import { showSplashScreen } from '../actions/showSplashScreen';
-import { loadNextMap } from '../actions/loadNextMap';
-import { startGame } from '../actions/startGame';
-import { startGameDebug } from '../actions/startGameDebug';
-import { pickupItem } from '../actions/pickupItem';
-import { useItem } from '../actions/useItem';
-import { ShootArrow } from '../entities/units/abilities/ShootArrow';
-import { Strafe } from '../entities/units/abilities/Strafe';
-import { NormalAttack } from '../entities/units/abilities/NormalAttack';
 import ImageFactory from '../graphics/images/ImageFactory';
 import GameScreenInputHandler from './screens/GameScreenInputHandler';
 import InventoryScreenInputHandler from './screens/InventoryScreenInputHandler';
@@ -29,20 +12,23 @@ import GameOverScreenInputHandler from './screens/GameOverScreenInputHandler';
 import MapScreenInputHandler from './screens/MapScreenInputHandler';
 import VictoryScreenInputHandler from './screens/VictoryScreenInputHandler';
 import HelpScreenInputHandler from './screens/HelpScreenInputHandler';
+import { checkNotNull } from '../utils/preconditions';
 
-type Props = Readonly<{
-  state: GameState
-}>;
+const screenHandlers: Record<GameScreen, InputHandlerType> = {
+  [GameScreen.GAME]:      GameScreenInputHandler,
+  [GameScreen.GAME_OVER]: GameOverScreenInputHandler,
+  [GameScreen.HELP]:      HelpScreenInputHandler,
+  [GameScreen.INVENTORY]: InventoryScreenInputHandler,
+  [GameScreen.MAP]:       MapScreenInputHandler,
+  [GameScreen.TITLE]:     TitleScreenInputHandler,
+  [GameScreen.VICTORY]:   VictoryScreenInputHandler
+};
 
 export default class InputHandler {
-  private readonly state: GameState;
-
   private busy: boolean;
   private eventTarget: HTMLElement | null;
 
-  constructor({ state }: Props) {
-    this.state = state;
-
+  constructor() {
     this.busy = false;
     this.eventTarget = null;
   }
@@ -73,27 +59,10 @@ export default class InputHandler {
 
     e.preventDefault();
 
-    const state = this.state;
+    const state = GameState.getInstance();
     const renderer = GameRenderer.getInstance();
     const imageFactory = ImageFactory.getInstance();
-    const handler: InputHandlerType = (() => {
-      switch (this.state.getScreen()) {
-        case GameScreen.GAME:
-          return GameScreenInputHandler;
-        case GameScreen.GAME_OVER:
-          return GameOverScreenInputHandler;
-        case GameScreen.HELP:
-          return HelpScreenInputHandler;
-        case GameScreen.INVENTORY:
-          return InventoryScreenInputHandler;
-        case GameScreen.MAP:
-          return MapScreenInputHandler;
-        case GameScreen.TITLE:
-          return TitleScreenInputHandler;
-        case GameScreen.VICTORY:
-          return VictoryScreenInputHandler;
-      }
-    })();
+    const handler: InputHandlerType = checkNotNull(screenHandlers[state.getScreen()]);
     await handler.handleKeyCommand(command, { state, renderer, imageFactory });
   };
 
