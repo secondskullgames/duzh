96599cb6ee4143f34151f7b5c1bef13b9dc46cb7
John Butler <john.butler@uconn.edu>
2023-05-29 12:43:51 -0400

    add missing files

---
+++ src/main/actions/attackObject.ts
@@ -0,0 +1,21 @@
+import Unit from '../entities/units/Unit';
+import Spawner, { SpawnerState } from '../entities/objects/Spawner';
+import { playSound } from '../sounds/playSound';
+import Sounds from '../sounds/Sounds';
+import GameObject, { ObjectType } from '../entities/objects/GameObject';
+import Activity from '../entities/units/Activity';
+import { LONG_SLEEP, sleep } from '../utils/promises';
+
+export const attackObject = async (
+  unit: Unit,
+  target: GameObject
+) => {
+  playSound(Sounds.SPECIAL_ATTACK);
+  unit.setActivity(Activity.ATTACKING, 1, unit.getDirection());
+  await sleep(LONG_SLEEP);
+  if (target.getObjectType() === ObjectType.SPAWNER) {
+    const spawner = target as Spawner;
+    spawner.setState(SpawnerState.DEAD);
+  }
+  unit.setActivity(Activity.STANDING, 1, unit.getDirection());
+}
