faf6f49942dab57a476459f5d64481b9f643fb2b
John Butler <john.butler@uconn.edu>
2023-06-18 12:54:14 -0400

    Add missing Blink ability

---
+++ src/main/entities/units/abilities/Blink.ts
@@ -0,0 +1,54 @@
+import Unit from '../Unit';
+import Coordinates from '../../../geometry/Coordinates';
+import { pointAt } from '../../../utils/geometry';
+import { playSound } from '../../../sounds/playSound';
+import Sounds from '../../../sounds/Sounds';
+import { type UnitAbility, type UnitAbilityContext } from './UnitAbility';
+import { moveUnit } from '../../../actions/moveUnit';
+import { AbilityName } from './AbilityName';
+
+const manaCost = 15;
+
+export const Blink: UnitAbility = {
+  name: AbilityName.BLINK,
+  manaCost,
+  icon: 'icon5',
+  use: async (
+    unit: Unit,
+    coordinates: Coordinates | null,
+    { state, renderer, imageFactory }: UnitAbilityContext
+  ) => {
+    if (!coordinates) {
+      throw new Error('Blink requires a target!');
+    }
+
+    const { dx, dy } = Coordinates.difference(unit.getCoordinates(), coordinates);
+
+    const map = state.getMap();
+
+    unit.setDirection(pointAt(unit.getCoordinates(), coordinates));
+
+    const distance = 2;
+    let { x, y } = unit.getCoordinates();
+    let moved = false;
+    for (let i = 0; i < distance; i++) {
+      x += dx;
+      y += dy;
+    }
+    if (map.contains({ x, y }) && !map.isBlocked({ x, y })) {
+      await moveUnit(
+        unit,
+        { x, y },
+        { state, renderer, imageFactory }
+      );
+      moved = true;
+      await renderer.render();
+    }
+
+    if (moved) {
+      unit.spendMana(manaCost);
+    } else {
+      playSound(Sounds.BLOCKED);
+    }
+  }
+}
