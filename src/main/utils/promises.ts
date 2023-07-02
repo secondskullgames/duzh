775d3b60274a7015e9e9d211668212ee8a477121
John Butler <john.butler@uconn.edu>
2023-07-02 13:50:18 -0400

    rm sleep logging

--- src/main/utils/promises.ts
+++ src/main/utils/promises.ts
@@ -1,8 +1,3 @@
-export const sleep = async (milliseconds: number): Promise<void> => new Promise<void>(resolve => {
-  const start = performance.now();
-  setTimeout(() => {
-    const end = performance.now();
-    console.log(`Sleep(${milliseconds}) took: ${end - start} ms`);
-    resolve();
-  }, milliseconds);
+export const sleep = async (milliseconds: number) => new Promise<void>(resolve => {
+  setTimeout(resolve, milliseconds);
 });
