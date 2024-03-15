2202c1d98b552bf925bb2f57e89721971d493042
John Butler <jbutler@tripadvisor.com>
2024-03-15 12:10:50 -0400

    split InputHandler -> @lib

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,13 +2,13 @@ import 'reflect-metadata';
 import { Debug } from './core/Debug';
 import { GameState, GameStateImpl } from './core/GameState';
 import GameRenderer from './graphics/renderers/GameRenderer';
-import InputHandler from './input/InputHandler';
 import { showSplashScreen } from './actions/showSplashScreen';
 import { FontFactory } from './graphics/Fonts';
 import { Feature } from './utils/features';
 import { Session, SessionImpl } from './core/Session';
 import { MapController, MapControllerImpl } from './maps/MapController';
 import MapSpec from '../models/MapSpec';
+import InputHandler from '@lib/input/InputHandler';
 import { AssetLoader, AssetLoaderImpl } from '@lib/assets/AssetLoader';
 import { createCanvas } from '@lib/utils/dom';
 import { checkNotNull } from '@lib/utils/preconditions';
@@ -17,6 +17,8 @@ import { GameConfig } from '@main/core/GameConfig';
 import mapSpecsJson from '@data/maps.json';
 import { Engine, EngineImpl } from '@main/core/Engine';
 import { FontBundle } from '@lib/graphics/Fonts';
+import ScreenHandlers from '@main/input/screens/ScreenHandlers';
+import { createInputHandler } from '@main/input/createInputHandler';
 import { Container } from 'inversify';
 
 type Props = Readonly<{
@@ -38,6 +40,11 @@ const setupContainer = async ({ gameConfig }: Props): Promise<Container> => {
   container.bind(AssetLoader).to(AssetLoaderImpl);
   container.bind(GameState).to(GameStateImpl);
   container.bind(MapController).to(MapControllerImpl);
+  container.bind(InputHandler).toDynamicValue(async context => {
+    const screenHandlers = context.container.get(ScreenHandlers);
+    const session = context.container.get<Session>(Session);
+    return createInputHandler({ session, screenHandlers });
+  });
   container.bind(Engine).to(EngineImpl);
   return container;
 };
@@ -56,7 +63,7 @@ const init = async ({ rootElement, gameConfig }: Props) => {
   const canvasGraphics = Graphics.forCanvas(canvas);
 
   const renderer = await container.getAsync(GameRenderer);
-  const inputHandler = container.get(InputHandler);
+  const inputHandler = await container.getAsync(InputHandler);
   inputHandler.addEventListener(canvas);
   if (Feature.isEnabled(Feature.DEBUG_BUTTONS)) {
     const debug = container.get(Debug);
