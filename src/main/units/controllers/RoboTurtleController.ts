c53fa1a9ba7301cd14b3f8678b92790c3f7b6ddf
John Butler <jbutler@tripadvisor.com>
2024-03-22 17:33:54 -0400

    turtle!

---
+++ src/main/units/controllers/RoboTurtleController.ts
@@ -0,0 +1,45 @@
+import StayOrder from '@main/units/orders/StayOrder';
+import UnitOrder from '@main/units/orders/UnitOrder';
+import { Session } from '@main/core/Session';
+import { GameState } from '@main/core/GameState';
+import Unit from '@main/units/Unit';
+import { UnitController } from '@main/units/controllers/UnitController';
+import { Coordinates } from '@lib/geometry/Coordinates';
+import { isBlocked } from '@main/maps/MapUtils';
+import { Direction } from '@lib/geometry/Direction';
+import { AttackMoveBehavior } from '@main/units/behaviors/AttackMoveBehavior';
+import { MoveOrder } from '@main/units/orders/MoveOrder';
+import { isHostile } from '@main/units/UnitUtils';
+import { AttackOrder } from '@main/units/orders/AttackOrder';
+import { canMove } from '@main/units/controllers/ControllerUtils';
+
+export class RoboTurtleController implements UnitController {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  issueOrder = (unit: Unit, _state: GameState, _session: Session): UnitOrder => {
+    if (!canMove(unit)) {
+      return new StayOrder();
+    }
+
+    const initialDirection = unit.getDirection();
+
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const nextCoordinates = Coordinates.plus(
+        unit.getCoordinates(),
+        unit.getDirection()
+      );
+      if (!isBlocked(unit.getMap(), nextCoordinates)) {
+        return new MoveOrder({ coordinates: nextCoordinates });
+      }
+      const targetUnit = unit.getMap().getUnit(nextCoordinates);
+      if (targetUnit && isHostile(unit, targetUnit)) {
+        return new AttackOrder({ direction: unit.getDirection() });
+      }
+
+      unit.setDirection(Direction.rotateClockwise(unit.getDirection()));
+      if (unit.getDirection() === initialDirection) {
+        return new StayOrder();
+      }
+    }
+  };
+}
