1a3a836e4cd700ddfd4844b1ae0a29f349dceb5c
jwbutler <jwb3861@gmail.com>
2025-11-19 12:00:44 -0500

    vite WIP

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import type { UserConfig } from 'vite'
+import path from "node:path";
+
+export default {
+  resolve: {
+    alias: {
+      "@data": path.resolve(__dirname, "data"),
+      "@main": path.resolve(__dirname, "src/main"),
+      "@lib": path.resolve(__dirname, "src/lib"),
+      "@models": path.resolve(__dirname, "src/models"),
+      "@png": path.resolve(__dirname, "png")
+    }
+  }
+} satisfies UserConfig;
